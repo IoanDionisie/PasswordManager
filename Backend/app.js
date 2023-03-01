ab7eaae1a04888f35007620766ffd112fdb98d77
Ioan Ciuciu <ioan.ciuciu@siemens.com>
2023-03-01 15:57:55 +0200

    Connected to mongoDb

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,21 @@
 // this is a javascript file
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+const mongoose  = require('./db/mongoose');
+
 // listen on port 3000
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
---
+++ Backend/db/mongoose.js
@@ -0,0 +1,19 @@
+// This file will handle connection logic to the MongoDB database
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const username = process.env.USER;
+const password = process.env.PASSWD;
+const cluster = process.env.CLUSTER;
+const dbname = process.env.DBNAME;
+const localUri = `mongodb://localhost:27017/${dbname}`;
+const uri = `mongodb+srv://${username}:${password}@${cluster}`;
+
+mongoose.set('strictQuery', false); 
+
+// Change the localUri with uri if you want to connect to the cloud database
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+module.exports = { mongoose };
